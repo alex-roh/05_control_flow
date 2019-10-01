da0ddad69db1bdc31d45e5ef9ee531d05ce717a9
eunyoungroh <roheunyoung@outlook.com>
2019-10-01 15:53:34 +0900

    실습 3

--- main.c
+++ main.c
@@ -25,13 +25,32 @@ int main(int argc, char *argv[]) {
 	
 	printf("다시 정수 하나를 입력하세요: \n");
 	scanf("%d", &num);
+	
 	if(num < 0)
 	{	
-		printf("입력한 정수의 절대값은 %d입니다.", -num); 
+		printf("입력한 정수의 절대값은 %d입니다.\n", -num); 
 	}
 	else
 	{
-		printf("입력한 정수의 절대값은 %d입니다.", num); 
+		printf("입력한 정수의 절대값은 %d입니다.\n", num); 
+	}
+	
+	// 실습 3 
+	
+	printf("다시 정수 하나를 입력하세요: \n");
+	scanf("%d", &num);
+	
+	if(num < 0)
+	{	
+		printf("입력한 정수는 음수입니다.\n"); 
+	}
+	else if(num > 0)
+	{
+		printf("입력한 정수는 양수입니다.\n"); 
+	}
+	else if(num == 0)
+	{
+		printf("입력한 정수는 0입니다.\n");
 	}
 	
 	return 0;
