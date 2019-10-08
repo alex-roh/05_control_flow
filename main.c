d1ceee2d87a426ae61f27b18d977984c5b8f0cd3
eunyoungroh <roheunyoung@outlook.com>
2019-10-08 15:08:18 +0900

    While

--- main.c
+++ main.c
@@ -53,6 +53,17 @@ int main(int argc, char *argv[]) {
 		printf("입력한 정수는 0입니다.\n");
 	}
 	
+	// 실습 4 
+	
+	int i = 0;
+	int meter = 0;
+	
+	while(i < 3){
+		meter = i * 1609;
+		printf("%d 마일은 %d 미터입니다.\n", i, meter);
+		i++;
+	}
+	
 	return 0;
 	
 }
