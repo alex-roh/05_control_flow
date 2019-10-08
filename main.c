5f40e521be525273011f23b279f1d32a1bec251e
eunyoungroh <roheunyoung@outlook.com>
2019-10-08 15:30:30 +0900

    실습 5

--- main.c
+++ main.c
@@ -58,11 +58,18 @@ int main(int argc, char *argv[]) {
 	int i = 0;
 	int meter = 0;
 	
-	while(i < 3){
+	while(i < 3)
+	{
+		
 		meter = i * 1609;
 		printf("%d 마일은 %d 미터입니다.\n", i, meter);
 		i++;
+	
 	}
+	
+	// 실습 5
+	for(i = 0; i < 10; i++)
+		printf("Hello World!\n"); 
 	
 	return 0;
 	
